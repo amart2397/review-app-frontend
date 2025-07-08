adebcf7b31938bbe00b163736e65405320ebc368
alex martinez <alexdmartinez.1997@gmail.com>
2025-07-07 22:09:31 -0500

    Add intial router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
-import "./App.css";
+import "./App.css"
+import router from "./routes"
+import { RouterProvider } from "react-router-dom"
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  )
 }
 
-export default App;
+export default App
